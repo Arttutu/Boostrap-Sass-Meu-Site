495a86fede63b7d316a6a75543f5f04a5c41796c
Arthur <arthurtt08@gmail.com>
2023-02-06 19:28:55 -0300

    animacai

---
+++ js/animacao-sections.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    $(".animate-on-scroll").each(function () {
+      var element = $(this)
+      if (
+        element.offset().top <
+        $(window).scrollTop() + $(window).height() * 0.85
+      ) {
+        element.addClass("animate")
+      }
+    })
+  })
+})
---
+++ js/banner-animacao.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var text = "Olá, Eu sou Arthur Gomes Desenvolvedor Front-end"
+  var barra = "|"
+  var typingSpeed = 100
+  var charIndex = 0
+
+  setInterval(function () {
+    if (charIndex < text.length) {
+      $("#animate-titulo").append(text[charIndex])
+      charIndex++
+    } else if (charIndex == text.length) {
+      charIndex++
+      $(".animate-barra").append(barra)
+    } else {
+      $(".animate-barra").fadeOut(1000)
+      $(".animate-barra").fadeIn(1000)
+    }
+  }, typingSpeed)
+})
